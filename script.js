a917abc6360991b86c99793fcbfa343cb33b13e1
Rakibul Islam <rirakib03@gmail.com>
2021-11-23 01:11:16 +0600

    app interface

---
+++ script.js
@@ -0,0 +1,19 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu_btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchFrom.classList.remove('active');
+}
+
+let searchFrom = document.querySelector('.search_from');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchFrom.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchFrom.classList.remove('active');
+    cartItem.classList.remove('active');
+}
